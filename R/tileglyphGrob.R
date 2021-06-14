fac2d7f1d2ac88f75ddedd3bb4e55328406f9998
J. Aravind <aravindjayaramanagri@gmail.com>
2021-06-14 21:30:57 +0200

    Added tileglyph documentation

--- R/tileglyphGrob.R
+++ R/tileglyphGrob.R
@@ -1,4 +1,75 @@
-
+#' Draw a Tile Glyph
+#'
+#' Uses \code{\link[grid]{Grid}} graphics to draw a tile glyph similar to
+#' 'autoglyph' \insertCite{beddow_shape_1990}{gglyph} or 'stripe glyph'
+#' \insertCite{fuchs_evaluation_2013}{gglyph}.
+#'
+#' @param x A numeric vector or unit object specifying x-locations.
+#' @param y A numeric vector or unit object specifying y-locations.
+#' @param z A numeric vector specifying the distance of star gylph points from
+#'   the center.
+#' @param size The size of glyphs.
+#' @param ratio The aspect ratio (height / width).
+#' @param nrow The number of rows.
+#' @param col The line colour.
+#' @param fill The fill colour.
+#' @param lwd The line width.
+#' @param alpha The alpha transparency value.
+#'
+#' @return A \code{\link[grid]{grob}} object.
+#'
+#' @importFrom grid rectGrob gpar
+#' @export
+#'
+#' @seealso \code{\link[gglyph]{geom_metroglyph}}
+#'
+#' @examples
+#' tg1 <- tileglyphGrob(x = 150, y = 150,
+#'                    z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7, 4.3),
+#'                    size = 30)
+#'
+#' tg2 <- tileglyphGrob(x = 450, y = 150,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7),
+#'                      size = 30)
+#'
+#' tg3 <- tileglyphGrob(x = 150, y = 250,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7, 4.3),
+#'                      size = 30, nrow = 2)
+#'
+#' tg4 <- tileglyphGrob(x = 450, y = 250,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7),
+#'                      size = 30, nrow = 2)
+#'
+#' tg5 <- tileglyphGrob(x = 150, y = 350,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7, 4.3),
+#'                      size = 30,
+#'                      fill = RColorBrewer::brewer.pal(7, "Dark2"))
+#'
+#' tg6 <- tileglyphGrob(x = 450, y = 350,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7),
+#'                      size = 30,
+#'                      fill = RColorBrewer::brewer.pal(7, "Dark2"))
+#'
+#' tg7 <- tileglyphGrob(x = 150, y = 450,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7, 4.3),
+#'                      size = 30, nrow = 2,
+#'                      fill = RColorBrewer::brewer.pal(7, "Dark2"))
+#'
+#' tg8 <- tileglyphGrob(x = 450, y = 450,
+#'                      z = c(4, 3.5, 2.7, 6.8, 3.4, 5.7),
+#'                      size = 30, nrow = 2,
+#'                      fill = RColorBrewer::brewer.pal(7, "Dark2"))
+#'
+#' grid::grid.newpage()
+#' grid::grid.draw(tg1)
+#' grid::grid.draw(tg2)
+#' grid::grid.draw(tg3)
+#' grid::grid.draw(tg4)
+#' grid::grid.draw(tg5)
+#' grid::grid.draw(tg6)
+#' grid::grid.draw(tg7)
+#' grid::grid.draw(tg8)
+#'
 tileglyphGrob <- function(x = .5, y = .5, z,
                           size = 10,
                           ratio = 1,
@@ -38,9 +109,13 @@ tileglyphGrob <- function(x = .5, y = .5, z,
 
   xylist <- dplyr::bind_rows(xylist)
 
-  grid::grid.points(x = xylist$xpos, y = xylist$ypos, pch =  1)
+  # grid::grid.points(x = xylist$xpos, y = xylist$ypos, pch =  1)
 
   grid::rectGrob(x= xylist$xpos, y = xylist$ypos,
                  width = width, height = height,
-                 default.units = "native", just = "centre")
+                 default.units = "native", just = "centre",
+                 gp = gpar(col = col,
+                           fill = fill,
+                           lwd = lwd,
+                           alpha = alpha))
 }
