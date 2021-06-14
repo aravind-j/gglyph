c1046bbd252808c962366c9997241c336fe5e5d2
J. Aravind <aravindjayaramanagri@gmail.com>
2021-06-15 01:20:13 +0200

    Add col2rgb as imports

--- R/utils.R
+++ R/utils.R
@@ -5,9 +5,11 @@ ggname <- utils::getFromNamespace("ggname", "ggplot2")
 
 
 # Check if a colour is valid
+
+#' @importFrom grDevices col2rgb
 iscolour <- function(x) {
   sapply(x, function(x) {
-    tryCatch(is.matrix(col2rgb(x)),
+    tryCatch(is.matrix(grDevices::col2rgb(x)),
              error = function(e) FALSE)
   })
 }
