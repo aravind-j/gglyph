041e1f8434e9939a5759ec8e6e68194b82a40cdf
J. Aravind <aravindjayaramanagri@gmail.com>
2021-06-15 00:01:35 +0200

    Add flip.axes argument

--- R/dotglyphGrob.R
+++ R/dotglyphGrob.R
@@ -6,21 +6,37 @@ dotglyphGrob <- function(x = .5, y = .5, z,
                          fill = NA,
                          lwd = 1,
                          alpha = 1,
-                         mirror = FALSE) {
+                         mirror = FALSE,
+                         flip.axes = FALSE) {
   z <- round(z)
   dimension <- length(z)
 
   # grid::grid.points(x = x, y = y, pch =  20)
 
-  xpos <- x + ((radius * 2) * seq(-(dimension-1)/2, (dimension-1)/2,
-                                  length.out = dimension))
-  ypos <- rep(y, dimension)
+  if (!flip.axes) {
+    xpos <- x + ((radius * 2) * seq(-(dimension-1)/2, (dimension-1)/2,
+                                    length.out = dimension))
+    ypos <- rep(y, dimension)
 
-  circx <- mapply(function(a, b) rep(a, b), xpos, z)
-  circy <- lapply(z, function(c) y - (1:c * (radius * 2)) + radius)
+    circx <- mapply(function(a, b) rep(a, b), xpos, z)
+    circy <- lapply(z, function(c) y - (1:c * (radius * 2)) + radius)
 
-  if (mirror) {
-    circy <- mapply(function(a, b) a + (radius * b[1]), circy, z)
+    if (mirror) {
+      circy <- mapply(function(a, b) a + (radius * b[1]), circy, z)
+    }
+
+  } else {
+    xpos <- rep(x, dimension)
+    ypos <- y - ((radius * 2) * seq(-(dimension-1)/2, (dimension-1)/2,
+                                    length.out = dimension))
+
+    circy <- mapply(function(a, b) rep(a, b), ypos, z)
+    circx <- lapply(z, function(c) x + (1:c * (radius * 2)) - radius)
+
+    if (mirror) {
+      circx <- mapply(function(a, b) a - (radius * b[1]), circx, z)
+    }
+
   }
 
   circx <- unlist(circx)
