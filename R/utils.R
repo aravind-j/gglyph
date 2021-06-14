d22541ec57caee118282b191fa4cb93e596583a0
J. Aravind <aravindjayaramanagri@gmail.com>
2021-06-14 18:24:25 +0200

    Add `iscolour`

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,13 @@
 
 # Get unexported functions from ggplot2
 ggname <- utils::getFromNamespace("ggname", "ggplot2")
+
+
+
+# Check if a colour is valid
+iscolour <- function(x) {
+  sapply(x, function(x) {
+    tryCatch(is.matrix(col2rgb(x)),
+             error = function(e) FALSE)
+  })
+}
