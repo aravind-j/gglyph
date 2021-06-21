75e25a543de1a6632517d84b8b1332d029cffe28
J. Aravind <aravindjayaramanagri@gmail.com>
2021-06-22 00:33:39 +0200

    template to fix draw.grid

---
+++ man-roxygen/draw.grid-arg.R
@@ -0,0 +1,3 @@
+#' @param draw.grid logical. If \code{TRUE}, grid points are plotted along the
+#'   whiskers if all the variables specified in \code{cols} are of type
+#'   \link[base]{factor}. Default is \code{FALSE}.
