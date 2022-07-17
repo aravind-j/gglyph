08ef213819499a4eead54b7a78f36dccc3f00670
J. Aravind <aravindjayaramanagri@gmail.com>
2022-07-18 01:59:58 +0200

    Fix argument documentation

--- R/gglyph.repel.control.R
+++ R/gglyph.repel.control.R
@@ -5,7 +5,39 @@
 #' (\code{geom_*()}) implemented in \code{gglyph} to repel the glyphs from each
 #' other.
 #'
-#' @template repel-arg
+#' @param nudge_x,nudge_y Horizontal and vertical adjustments to nudge the
+#'   starting position of each text label. The units for \code{nudge_x} and
+#'   \code{nudge_y} are the same as for the data units on the x-axis and y-axis.
+#' @param xlim,ylim Limits for the x and y axes. Text labels will be constrained
+#'   to these limits. By default, text labels are constrained to the entire plot
+#'   area.
+#' @param box.padding Amount of padding around bounding box, as unit or number.
+#'   Defaults to 0.25. (Default unit is lines, but other units can be specified
+#'   by passing \code{unit(x, "units")}).
+#' @param point.padding Amount of padding around labeled point, as unit or
+#'   number. Defaults to 0. (Default unit is lines, but other units can be
+#'   specified by passing \code{unit(x, "units")}).
+#' @param min.segment.length Skip drawing segments shorter than this, as unit or
+#'   number. Defaults to 0.5. (Default unit is lines, but other units can be
+#'   specified by passing \code{unit(x, "units")}).
+#' @param arrow specification for arrow heads, as created by
+#'   \code{\link[grid]{arrow}}
+#' @param force Force of repulsion between overlapping text labels. Defaults to
+#'   1.
+#' @param force_pull Force of attraction between a text label and its
+#'   corresponding data point. Defaults to 1.
+#' @param max.time Maximum number of seconds to try to resolve overlaps.
+#'   Defaults to 0.5.
+#' @param max.iter Maximum number of iterations to try to resolve overlaps.
+#'   Defaults to 10000.
+#' @param max.overlaps Exclude text labels that overlap too many things.
+#'   Defaults to 10.
+#' @param direction "both", "x", or "y" -- direction in which to adjust position
+#'   of labels
+#' @param seed Random seed passed to \code{\link[base]{set.seed}}. Defaults to
+#'   \code{NA}, which means that \code{set.seed} will not be called.
+#' @param verbose If \code{TRUE}, some diagnostics of the repel algorithm are
+#'   printed
 #'
 #' @return A list with the following components to control the repel algorithm
 #'   corresponding to the same in \strong{Arguments}. \item{box.padding}{}
@@ -47,7 +79,6 @@ gglyph.repel.control <- function(box.padding = 0.25,
                                  nudge_y = 0,
                                  xlim = c(NA, NA),
                                  ylim = c(NA, NA),
-                                 na.rm = FALSE,
                                  direction = c("both","y","x"),
                                  seed = NA,
                                  verbose = FALSE) {
@@ -67,7 +98,6 @@ gglyph.repel.control <- function(box.padding = 0.25,
               nudge_y = nudge_y,
               xlim = xlim,
               ylim = ylim,
-              na.rm = na.rm,
               direction = direction,
               seed = NA,
               verbose = FALSE)
--- man-roxygen/repel-arg.R
+++ man-roxygen/repel-arg.R
@@ -4,36 +4,5 @@
 #'   Ignored if \code{repel = FALSE}. See
 #'   \code{\link[gglyph]{gglyph.repel.control}} for details  on the various
 #'   control parameters.
-#' @param nudge_x,nudge_y Horizontal and vertical adjustments to nudge the
-#'   starting position of each text label. The units for \code{nudge_x} and
-#'   \code{nudge_y} are the same as for the data units on the x-axis and y-axis.
-#' @param xlim,ylim Limits for the x and y axes. Text labels will be constrained
-#'   to these limits. By default, text labels are constrained to the entire plot
-#'   area.
-#' @param box.padding Amount of padding around bounding box, as unit or number.
-#'   Defaults to 0.25. (Default unit is lines, but other units can be specified
-#'   by passing \code{unit(x, "units")}).
-#' @param point.padding Amount of padding around labeled point, as unit or
-#'   number. Defaults to 0. (Default unit is lines, but other units can be
-#'   specified by passing \code{unit(x, "units")}).
-#' @param min.segment.length Skip drawing segments shorter than this, as unit or
-#'   number. Defaults to 0.5. (Default unit is lines, but other units can be
-#'   specified by passing \code{unit(x, "units")}).
-#' @param arrow specification for arrow heads, as created by
-#'   \code{\link[grid]{arrow}}
-#' @param force Force of repulsion between overlapping text labels. Defaults to
-#'   1.
-#' @param force_pull Force of attraction between a text label and its
-#'   corresponding data point. Defaults to 1.
-#' @param max.time Maximum number of seconds to try to resolve overlaps.
-#'   Defaults to 0.5.
-#' @param max.iter Maximum number of iterations to try to resolve overlaps.
-#'   Defaults to 10000.
-#' @param max.overlaps Exclude text labels that overlap too many things.
-#'   Defaults to 10.
-#' @param direction "both", "x", or "y" -- direction in which to adjust position
-#'   of labels
-#' @param seed Random seed passed to \code{\link[base]{set.seed}}. Defaults to
-#'   \code{NA}, which means that \code{set.seed} will not be called.
-#' @param verbose If \code{TRUE}, some diagnostics of the repel algorithm are
-#'   printed
+#'
+
